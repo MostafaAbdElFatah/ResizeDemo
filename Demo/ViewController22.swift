582ec7ff3a37fd6ebd2286fb7c980ccf8e6fe4e7
MostafaAbdElFatah <m.abdelfatah.fci@gmail.com>
2019-12-29 13:29:23 +0200

    slide out view with pan

--- Demo/ViewController22.swift
+++ Demo/ViewController22.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
+
+enum SlideOutState {
+   case Collapsed
+   case Expanded
+}
+
 class ViewController: UIViewController {
 
+
+    
     @IBOutlet weak var blueView: UIView!
     @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var greenViewConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +35,6 @@ class ViewController: UIViewController {
         let translation:CGPoint = sender.translation(in: self.blueView)
         self.blueView.center = CGPoint(x: self.blueView.center.x + translation.x, y: self.blueView.center.y + translation.y)
         self.redButton.center = CGPoint(x: self.blueView.frame.origin.x + self.blueView.frame.size.width, y: self.blueView.frame.origin.y + self.blueView.frame.size.height)
-        
         sender.setTranslation(CGPoint.zero, in: self.view)
     }
     
@@ -39,5 +48,43 @@ class ViewController: UIViewController {
         sender.setTranslation(CGPoint.zero, in: self.view)
         sender.view?.center = translation
     }
+    
+    
+    @IBAction func greenViewPanned(_ sender: UIPanGestureRecognizer) {
+
+        let velocity = sender.velocity(in: self.view)
+        let translation = sender.translation(in: self.view)
+        
+        switch(sender.state) {
+        case .changed:
+            let constant = greenView.frame.origin.y + translation.y
+            if constant < self.view.frame.height - 200 || constant > self.view.frame.height - 50 {
+                return
+            }
+            if self.greenViewConstraint.constant - abs(translation.y) > 150 || self.greenViewConstraint.constant + abs(translation.y) < 0 {
+                return
+            }
+            UIView.animate(withDuration: 0.5) {
+                if velocity.y > 0{
+                    self.greenViewConstraint.constant = self.greenViewConstraint.constant + abs(translation.y)
+                    self.view.layoutIfNeeded()
+                }else{
+                    self.greenViewConstraint.constant = self.greenViewConstraint.constant - abs(translation.y)
+                    self.view.layoutIfNeeded()
+                }
+                
+                //self.greenView.center.y = self.greenView.center.y + translation.y
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            }
+        case .ended: break
+            
+        default:
+            break
+        }
+    }
+    
+   
+    
+    
 }
 
